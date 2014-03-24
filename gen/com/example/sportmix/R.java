0d10dffc81eb8daf560b217098c0598ceb073964
SamDeshpande <deshpande.71@osu.edu>
2014-03-23 22:58:09 -0400

    My first commit

--- gen/com/example/sportmix/R.java
+++ gen/com/example/sportmix/R.java
@@ -8,6 +8,9 @@
 package com.example.sportmix;
 
 public final class R {
+    public static final class array {
+        public static final int prefrenceSports=0x7f060000;
+    }
     public static final class attr {
         /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
 or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
@@ -177,9 +180,11 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int sportbutton=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090002;
-        public static final int pager=0x7f090000;
-        public static final int textView1=0x7f090001;
+        public static final int action_settings=0x7f0a0004;
+        public static final int addPreferenceButton=0x7f0a0001;
+        public static final int pager=0x7f0a0000;
+        public static final int preferenceListView=0x7f0a0002;
+        public static final int textView1=0x7f0a0003;
     }
     public static final class layout {
         public static final int activity_fullscreen=0x7f030000;
@@ -192,17 +197,17 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int splash=0x7f030007;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
-        public static final int transp=0x7f080001;
+        public static final int main=0x7f090000;
+        public static final int transp=0x7f090001;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int dummy_button=0x7f060004;
-        public static final int dummy_content=0x7f060005;
-        public static final int hello_world=0x7f060002;
-        public static final int title_activity_fullscreen=0x7f060003;
-        public static final int title_activity_transp=0x7f060006;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int dummy_button=0x7f070004;
+        public static final int dummy_content=0x7f070005;
+        public static final int hello_world=0x7f070002;
+        public static final int title_activity_fullscreen=0x7f070003;
+        public static final int title_activity_transp=0x7f070006;
     }
     public static final class style {
         /** 
@@ -229,16 +234,16 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070002;
-        public static final int ButtonBar=0x7f070004;
-        public static final int ButtonBarButton=0x7f070005;
-        public static final int FullscreenActionBarStyle=0x7f070006;
-        public static final int FullscreenTheme=0x7f070003;
-        public static final int Theme_Transparent=0x7f070001;
+        public static final int AppTheme=0x7f080002;
+        public static final int ButtonBar=0x7f080004;
+        public static final int ButtonBarButton=0x7f080005;
+        public static final int FullscreenActionBarStyle=0x7f080006;
+        public static final int FullscreenTheme=0x7f080003;
+        public static final int Theme_Transparent=0x7f080001;
     }
     public static final class styleable {
         /** 
--- src/com/example/sportmix/PreferencesFragment.java
+++ src/com/example/sportmix/PreferencesFragment.java
@@ -1,18 +1,59 @@
 package com.example.sportmix;
 import com.example.sportmix.R;
+import com.google.gwt.user.client.ui.Button;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
 public class PreferencesFragment extends Fragment {
+	
+	private ListView preferenceSportList;
+	private View addPreferenceButton;
+	
+	
+	private String[] preferenceSportString;
+	static final String[] preferenceFootballString = new String[] {"FC Barcelona","Real Madrid","Manchester United"};
+	private String[] dataSource;
+	
 	 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
  
         View rootView = inflater.inflate(R.layout.fragment_preferences, container, false);
-         
+        
+        preferenceSportList = (ListView) rootView.findViewById(R.id.preferenceListView);
+        addPreferenceButton =  getActivity().findViewById(R.id.addPreferenceButton);
+        
+        preferenceSportString = getResources().getStringArray(R.array.prefrenceSports);
+        dataSource = preferenceSportString;
+        
+        final ArrayAdapter<String> preferenceAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1,dataSource);
+        
+        preferenceSportList.setAdapter(preferenceAdapter);
+        
+        preferenceSportList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				// TODO Auto-generated method stub
+				
+		        dataSource = preferenceFootballString;
+		        preferenceAdapter.notifyDataSetChanged();
+		       Toast.makeText(getActivity(),preferenceSportString[position], Toast.LENGTH_SHORT).show();
+			}
+        	
+        	
+		});
+        
         return rootView;
     }
 }
